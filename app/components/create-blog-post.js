efa8490a6d02b94c43a4dc0e9e6a631e81a8705c
James Futhey <futhey@gmail.com>
2015-05-06 22:42:17 -0700

    resolves jshint error
    completely superfluous way of resolving the jshint warning about jQuery

--- app/components/create-blog-post.js
+++ app/components/create-blog-post.js
@@ -10,7 +10,7 @@ export default Ember.Component.extend({
         createdDate: new Date()
       });
       post.save();
-      $("#NewPost")[0].reset();
+      Ember.$("#NewPost")[0].reset();
     }
   }
 });
